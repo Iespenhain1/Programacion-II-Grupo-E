e25fbd020ddc9660b828b532a2017114c0ba01a3
Iespenhain1 <isa.espenhain@gmail.com>
2025-08-27 08:35:57 -0400

    Add files via upload

---
+++ SpinBox.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def mostrarEdad():
+    tk.messagebox.showinfo("Edad", f"La edad seleccionada es: {spin.get()}")
+    
+ventana=tk.Tk()
+ventana.title("Ejemplo con edad")
+ventana.configure(bg="LightBlue")
+labelEdad=tk.Label(ventana, text="Edad")
+labelEdad.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+spin=tk.Spinbox(ventana, from_=1, to=10)
+spin.grid(row=0, column=1, padx=10, pady=10)
+boton=tk.Button(ventana, text="Obtener valor", command=mostrarEdad)
+boton.grid(row=1, column=0, padx=10, pady=10)
+
+
+ventana.mainloop()
